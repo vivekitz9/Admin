a2e5b4b9900ebc451a83b2d243bf02d8d6bff32e
abhishek <shanti7482@gmail.com>
2025-02-14 20:55:15 +0530

    Add Banners Add section

---
+++ src/styles/Banner/banner.js
@@ -0,0 +1,42 @@
+const bannerStyle = {
+  mainContianer: {
+    pt: 4,
+  },
+  formContainer: {
+    mt: 3,
+    p: 3,
+    mb: 4,
+  },
+  heading: {
+    mb: 3,
+  },
+  formContent: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 3,
+  },
+  rightContainer: {
+    display: "flex",
+    flexDirection: { xs: "column", md: "row" },
+    gap: 3,
+  },
+  cardContant: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cardAction: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingBottom: 2,
+  },
+  imageContainer: {
+    display: "flex",
+    alignItem: "center",
+    justifyContent: "center",
+    marginTop: "2rem",
+  },
+};
+
+export default bannerStyle;
